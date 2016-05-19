c8ec99d9a2db4f3e11c550137bb02dd19af273d1
nielo <nielowait@gmail.com>
2016-05-19 12:00:35 +0200

    Testing GameSpark CloudCode to Repo

---
+++ event/LR.js
@@ -0,0 +1,9 @@
+// ====================================================================================================
+//
+// Cloud Code for LR, write your code here to customise the GameSparks platform.
+//
+// For details of the GameSparks Cloud Code API see https://portal.gamesparks.net/docs.htm			
+//
+// ====================================================================================================
+//Something goes here!
+Spark.getPlayer().setPrivateData("TestValue", 1001);
